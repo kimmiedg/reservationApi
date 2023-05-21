e203f177018faa4fa5920318698b468142c3235e
kim <kdegracia.dnc@gmail.com>
2023-05-21 12:01:22 +0800

    Added shoulda-matchers for validation test

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,9 @@
 class Reservation < ApplicationRecord
   belongs_to :guest
+  validates_uniqueness_of :reservation_code
+  validates_presence_of :reservation_code, :currency, :status, :start_date, :end_date,
+                        :no_of_nights, :no_of_guests, :no_of_infants, :no_of_adults,
+                        :no_of_children, :total_price, :payout_amt, :security_price, :guest_id
+
+
 end
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,10 @@ module ReservationApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    initializer(:remove_action_mailbox_and_activestorage_routes, after: :add_routing_paths) { |app|
+      app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}
+      app.routes_reloader.paths.delete_if {|path| path =~ /actionmailbox/ }
+    }
 
     # Configuration for the application, engines, and railties goes here.
     #
